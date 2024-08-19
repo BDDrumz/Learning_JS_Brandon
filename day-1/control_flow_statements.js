295963254923223ba963013f93a070d8f2c9750e
BDDrumz <brandoneazzi69@gmail.com>
2024-08-19 16:19:38 +0100

    update

--- day-1/control_flow_statements.js
+++ day-1/control_flow_statements.js
@@ -10,18 +10,4 @@ if (condition1) {
     statementLast;
   }
 */
-//example
-function checkData() {
-    if (document.form1.threeChar.value.length === 3) {
-      return true;
-    } else {
-      alert(
-        `Enter exactly three characters. ${document.form1.threeChar.value} is not valid.`,
-      );
-      return false;
-    }
-  }
-  
-  
 
-  
