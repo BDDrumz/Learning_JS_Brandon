087521fedb4eb8b962e57908852fdd94fbe0ca64
BDDrumz <brandoneazzi69@gmail.com>
2024-08-21 10:16:55 +0100

    array

--- day-3/array.js
+++ day-3/array.js
@@ -1,3 +1,13 @@
+//Relationship between length and numerical properties
+
 const fruits = [];
 fruits.push("banana", "apple", "peach");
 console.log(fruits.length); // 3
+
+//index is outside the current bounds of the array
+fruits[5] = "mango";
+console.log(fruits[5]); // 'mango'
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 6
+
+//
