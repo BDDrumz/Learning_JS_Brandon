328bb1ff83333c8a900f3654dc824c3006c7d2ad
BDDrumz <brandoneazzi69@gmail.com>
2024-08-21 10:03:05 +0100

    length/num property

--- Day-2/function.js
+++ Day-2/function.js
@@ -29,34 +29,34 @@ let A=area();
 console.log("The area is ",A);
 
 const person = {
-//   fullName: function () {
-//     return this.firstName + " " + this.lastName;  
+ fullName: function () {
+    return this.firstName + " " + this.lastName;  
 
-//     }
-//   }
-//   const person1 ={
-//     firstName:"Daniel",
-//     lastName: "Dior",
+     }
+   }
+  const person1 ={
+    firstName:"Daniel",
+    lastName: "Dior",
    
-//   }
-//   const person2 = {
-//     firstName: "BD",
-//     lastName: "Drumz"
-//   }
-// person.fullName.call(person1);
-//   console.log(person.fullName.call(person1)); 
-//   */
-// //function apply: Accepts argument in array
+  }
+  const person2 = {
+    firstName: "BD",
+    lastName: "Drumz"
+  }
+person.fullName.call(person1);
+  console.log(person.fullName.call(person1)); 
+  
+//function apply: Accepts argument in array
 
-// const person = {
-//   fullName: function (city, country) {
-//     return this.firstName + " " + this.lastName + ","+ city +","+ country;  
+const person = {
+  fullName: function (city, country) {
+    return this.firstName + " " + this.lastName + ","+ city +","+ country;  
 
-//     }
-//   }
-//   const person1 ={
-//     firstName:"Daniel",
-//     lastName: "Dior",   
-//   }
-// person.fullName.apply(person1, ["Buea", "Camerron"]);
-// console.log(person.fullName.apply(person1, ["Buea", "Camerron"]));
+    }
+  }
+  const person1 ={
+    firstName:"Daniel",
+    lastName: "Dior",   
+  }
+person.fullName.apply(person1, ["Buea", "Camerron"]);
+console.log(person.fullName.apply(person1, ["Buea", "Camerron"]));
