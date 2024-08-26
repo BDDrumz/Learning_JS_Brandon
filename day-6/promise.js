666551a0546435da56982939bdb248940ddab23d
BDDrumz <brandoneazzi69@gmail.com>
2024-08-26 12:17:45 +0100

    ex1

--- day-6/promise.js
+++ day-6/promise.js
@@ -1,16 +1,25 @@
-function myDisplayer(some){
-    document.getElementById("demo").innerHTML = some;
-}
+// function myDisplayer(some){
+//     document.getElementById("demo").innerHTML = some;
+// }
+// let myPromise = new Promise(function(myResolve, myReject){
+//     let x = 10;
+//     if(x == 0){
+//         myResolve("OK");
+//     }else{
+//         myReject("error");
+//     }
+// });
+// myPromise.then(
+//     function(value) {myDisplayer(value);},
+//     function(error) {myDisplayer(error);}
+// );
+// console.log(myResolve);
+
+//example
+
 let myPromise = new Promise(function(myResolve, myReject){
-    let x = 10;
-    if(x == 0){
-        myResolve("OK");
-    }else{
-        myReject("error");
-    }
+setTimeout(function() {myResolve("I love you !!");}, 3000);    
 });
-myPromise.then(
-    function(value) {myDisplayer(value);},
-    function(error) {myDisplayer(error);}
-);
-console.log(myResolve);
+myPromise.then(function(value){
+    document.getElementById("demo").innerHTML = value;
+});
