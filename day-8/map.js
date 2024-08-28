c6ca3459c0086bc70db26ed5a3fc83a8919bfb88
BDDrumz <brandoneazzi69@gmail.com>
2024-08-28 12:35:42 +0100

    Iterate over a map

--- day-8/map.js
+++ day-8/map.js
@@ -1,13 +1,35 @@
-let map = new Map();
-map.set('1', 'str1');
-map.set(1, 'num1');
-map.set(true, 'bool1')
-alert(map.get(1));
-alert(map.get('1'));
-alert(map.size);
+// let map = new Map();
+// map.set('1', 'str1')
+// .set(1, 'num1')
+// .set(true, 'bool1');
+// alert(map.get(1));
+// alert(map.get('1'));
+// alert(map.size);
 
-// using objets as keys
-let john = {name: "John"};
-let visitsCountMap = new Map();
-visitsCountMap.set(john, 123);
-alert( visitsCountMap.get(john));
+// // using objets as keys
+// let john = {name: "John"};
+// let visitsCountMap = new Map();
+// visitsCountMap.set(john, 123);
+// alert( visitsCountMap.get(john));
+// //example
+// john = {name: "John"};
+// let ben = {name: "Ben"};
+// let visitsCountObj = {};
+// visitsCountObj.set(john, 234);
+// visitsCountObj.set(ben, 123);
+// alert(visitsCountObj["[object, Object]"]);
+//Iterate over a map
+let recipeMap = new Map([
+    ['cucumber', 500],
+    ['tomatoes', 350],
+    ['onion', 50],
+]);
+for (let vegetable of recipeMap.keys()){
+    alert(vegetable); 
+}
+for ( let amount of recipeMap.values()) {
+    alert(amount);
+}
+for ( let entry of recipeMap) {
+    alert(entry);
+}
