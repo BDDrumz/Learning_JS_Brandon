f7f8f8f78797ab27b4ffe129be866cf4e429ca95
BDDrumz <brandoneazzi69@gmail.com>
2024-08-28 14:11:30 +0100

    iterate over set

--- day-8/set.js
+++ day-8/set.js
@@ -1,17 +1,24 @@
-let set = new Set();
-let john = {name: "john"};
-let peter = {name: "Peter"};
-let mary = {name: "Mary"};
+// let set = new Set();
+// let john = {name: "john"};
+// let peter = {name: "Peter"};
+// let mary = {name: "Mary"};
 
-set.add(john);
-set.add(peter);
-set.add(mary);
-set.add(john);
-set.add(mary); 
+// set.add(john);
+// set.add(peter);
+// set.add(mary);
+// set.add(john);
+// set.add(mary); 
 
-alert(set.size)
+// alert(set.size)
 
-for (let user of set){
-    alert(user.name);
-}
-console.log(user.name);
+// for (let user of set){
+//     alert(user.name);
+// }
+// console.log(user.name);
+
+//Iteration over set
+let set = new Set(["oranges", "apples", "bananas"]);
+for ( let value of set) alert(value);
+set.forEach((value, valueAgain, set) =>{
+    alert(value);
+});
