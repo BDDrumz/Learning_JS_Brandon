1f3a58f7fe0ea7bca35d0d0ac32243c7b29fde54
BDDrumz <brandoneazzi69@gmail.com>
2024-08-29 14:51:06 +0100

    scheduler app

--- day-8/schedule.js
+++ day-8/schedule.js
@@ -1,33 +1,8 @@
-// const task = [
-//     {title: "Meeting with team", time: "9:00am - 10:00am", description: "Discuss on new project"},
-//     {title: "Work on project report", time: "4:00pm-7:00pm", description: "Draft the final report for the project."}
-// ];
-
-// document.addEventListener('DOMContentLoaded', () =>{
-//     const div = document.querySelector('.schedule')
-//     const input =document.getElementById('name')
-//     const newDiv = document.createElement('div')
-
-//     const ctaBtn = document.getElementById("btn")
-
-//     ctaBtn.addEventListener("click", () => {
-//         div.appendChild(newDiv)
-//         newDiv.innerHTML = input.value
-//     })
-
-// })
 
 document.addEventListener('DOMContentLoaded', () => {
     const taskForm = document.getElementById('taskForm');
     const taskList = document.getElementById('taskList');
     const message = document.getElementById('message');
-    // const newDiv = document.createElement('div');
-
-    // const ctaBtn = document.getElementById("btn");
-    // ctaBtn.addEventListener("click"  , () => {
-    //     div.appendChild(newDiv)
-    //     newDiv.innerHTML = input.value;
-    // });
 
     const taskNames = new Set();
     const task = new Map();
