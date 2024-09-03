2de50e1ea7ef9a5fc9b6f690153175f970a64cf1
BDDrumz <brandoneazzi69@gmail.com>
2024-09-03 10:04:52 +0100

    Todo

---
+++ day-11/first-react/src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+        <p>Go to school</p>
+        <div>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Todo
