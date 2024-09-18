3b1bc83ebe4af27211c31851a12e70a91c61fae5
BDDRUMZ <brandoneazzi69@gmail.com>
2024-09-18 12:45:08 +0100

    update

--- day-14/admin-site/src/components/index.jsx
+++ day-14/admin-site/src/components/index.jsx
@@ -2,8 +2,69 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './dashboard.css'
 import '../App.css'
-
+import { CategoryScale } from "chart.js"
+import { Chart, registerables } from "chart.js"
+import { Line, Bar, } from "react-chartjs-2"
+import {
+  UsersRound, ShoppingCart, MoveDown, MoveUp,
+  UserRoundPlus, Users} from "lucide-react"
+import Data from './profile-data'
+Chart.register(CategoryScale, ...registerables);
 function Index() {
+  const data = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Dec'],
+    datasets: [
+      {
+        label: 'Users',
+        data: [75, 78, 79, 48, 30, 15, 40, 80, 74, 24, 100,],
+        backgroundColor: 'skyblue',
+        borderColor: 'white',
+        borderWidth: 0.3,
+        tension: 0.1
+      },
+      {
+        label: 'New Users',
+        date: [75, 78, 79, 45, 30, 19, 45, 90, 70, 25, 15, 100],
+        backgroundColor: 'rgb(1, 183, 255)',
+        borderColor: 'white',
+        borderWidth: 0.3,
+        tension: 0.1
+      },
+    ],
+  };
+  const data_2 = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    datasets: [
+      {
+        label: 'Datasets',
+        data: [70, 73, 68, 63, 65],
+        fill: true,
+        borderColor: 'rgb(75,192,192)',
+        tension: 0.1
+      },
+      {
+        label: 'New Users',
+        date: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: 'rgb(1, 183, 255)',
+        borderColor: 'white',
+        borderWidth: 0.3,
+        fill: true,
+        tension: 0.1
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: 'category',
+      },
+      y: {
+        type: 'linear',
+      }
+    },
+  };
+
   return (
     <div className='dashboard-main'>
       <div>
@@ -13,23 +74,32 @@ function Index() {
       <div className='sale-main'>
         <div className='sale-left'>
           <div className='s-left1'>
-            Sale
+            <h3> Sale</h3>
+            <p>January - July 2023</p>
+            <div> <Line data={data_2} options={options} /></div>
           </div>
 
           <div className='s-left2'>
 
             <div className='s-left3'>
-              Customers
+              <div className='left-icons-main'><p>Customers</p><UsersRound className='left-icons' />
+              </div>
+              <h3 className='left-text'>44.725</h3>
+              <p className='left-text1'>(-12.4% <MoveDown className='arrows' />)</p>
             </div>
             <div className='s-left4'>
-              Orders
+              <div className='left-icons-main'> <p>Orders </p><ShoppingCart className='left-icons' /></div>
+              <h3 className='left-text'>385</h3>
+              <p className='left-text2'  >(17.2% <MoveUp className='arrows' />) </p>
             </div>
 
           </div>
 
         </div>
         <div className='sale-right'>
-          Traffic
+          <h3>Traffic</h3>
+          <p>January 01, 2023 - December 31, 2023</p>
+          <div className='bar-graph'><Bar data={data} options={options} /></div>
         </div>
       </div>
 
@@ -37,8 +107,34 @@ function Index() {
 
         <div className='users-left'>
           <div className="users">
-            Users
+            <div> <h2>Users</h2>
+              <p>1.232.150 registered users</p></div>
+            <div><button className='btn_add'> <UserRoundPlus className='add_btn' />Add New User</button></div>
+
+           
           </div>
+          <div className='users-main-profile'>
+              <div className='profile-head'>
+                <div className='userDiv'><Users /></div>
+                <div className='usersDiv'><h3>User</h3></div>
+                <div className='countryDiv'><h3>Country</h3></div>
+                <div className='usageDiv'><h3>Usage</h3></div>
+                <div className='activityDiv'><h3>Activity</h3></div>
+              </div>
+              <div className='profile-info'>
+               <div><img src="#" /></div>
+               <div className='usersDiv'>Branzy</div>
+               <div className='countryDiv'>Cameroon</div>
+               <div className='usageDiv'>50%</div>
+               <div className='activityDiv'>10sec</div>
+                
+                {/* {Data.map(user=>(
+                  <div>
+                  <div className="profile_pic"> <img src="#"/> </div>
+                  <div className="users_name"></div>
+                  </div>                ))} */}
+              </div>
+            </div>
         </div>
 
         <div className="users-right">
---
+++ day-14/admin-site/src/components/profile-data.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import
+const Data =[
+    {
+        // 'pic': '',
+        'name': 'Mac',
+        'type':'new',
+        'registered':'Jan 1, 2023',
+        'country':'cam',
+        'usage': '50%',
+        'activity': '10sec ago'
+    }
+]
+export default Data;
